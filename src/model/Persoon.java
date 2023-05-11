495d95f6689fe33fd1faa2a71b12c2db38202971
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-05-11 09:40:03 +0200

    Opdracht 1.1

--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -5,6 +5,9 @@ package model;
  * Een persoon binnen ons bedrijf
  */
 public class Persoon {
+    private static final String DEFAULT_WOONPLAATS = "Onbekend";
+    private static final int DEFAULT_MAAND_SALARIS = 0;
+    private static final String DEFAULT_NAAM = "Onbekend";
     public static int aantalPersonen = 0;
 
     public int personeelsNummer;
@@ -17,5 +20,23 @@ public class Persoon {
         this.woonplaats = woonplaats;
         this.maandSalaris = maandSalaris;
         this.personeelsNummer = ++aantalPersonen;
+    }
+
+    public Persoon(String naam) {
+        this.naam = naam;
+        this.woonplaats = DEFAULT_WOONPLAATS;
+        this.maandSalaris = DEFAULT_MAAND_SALARIS;
+        this.personeelsNummer = ++aantalPersonen;
+    }
+
+    public Persoon() {
+        this.naam = DEFAULT_NAAM;
+        this.woonplaats = DEFAULT_WOONPLAATS;
+        this.maandSalaris = DEFAULT_MAAND_SALARIS;
+        this.personeelsNummer = ++aantalPersonen;
+    }
+
+    public double berekenJaarInkomen() {
+        return 12 * maandSalaris;
     }
 }
