b36b82219332d3043afc6841ad583031b8fe8633
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-05-11 14:38:31 +0200

    Opdracht 2.1

--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -5,38 +5,63 @@ package model;
  * Een persoon binnen ons bedrijf
  */
 public class Persoon {
+    private static final double GRENSWAARDE_BONUS = 4500.0;
+    private static int aantalPersonen = 0;
+
     private static final String DEFAULT_WOONPLAATS = "Onbekend";
     private static final int DEFAULT_MAAND_SALARIS = 0;
     private static final String DEFAULT_NAAM = "Onbekend";
-    public static int aantalPersonen = 0;
 
-    public int personeelsNummer;
-    public String naam;
-    public String woonplaats;
-    public double maandSalaris;
+    private int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandSalaris;
 
     public Persoon(String naam, String woonplaats, double maandSalaris) {
         this.naam = naam;
         this.woonplaats = woonplaats;
-        this.maandSalaris = maandSalaris;
+        setMaandSalaris(maandSalaris);
         this.personeelsNummer = ++aantalPersonen;
     }
 
     public Persoon(String naam) {
-        this.naam = naam;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandSalaris = DEFAULT_MAAND_SALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(naam, DEFAULT_WOONPLAATS, DEFAULT_MAAND_SALARIS);
     }
 
     public Persoon() {
-        this.naam = DEFAULT_NAAM;
-        this.woonplaats = DEFAULT_WOONPLAATS;
-        this.maandSalaris = DEFAULT_MAAND_SALARIS;
-        this.personeelsNummer = ++aantalPersonen;
+        this(DEFAULT_NAAM);
     }
 
     public double berekenJaarInkomen() {
         return 12 * maandSalaris;
+    }
+
+    public boolean heeftRechtOpBonus() {
+        return maandSalaris >= GRENSWAARDE_BONUS;
+    }
+
+    public void setMaandSalaris(double maandSalaris) {
+        if (maandSalaris < 0) {
+            System.err.println("Het maandsalaris moet 0 of groter zijn");
+            this.maandSalaris = DEFAULT_MAAND_SALARIS;
+        } else {
+            this.maandSalaris = maandSalaris;
+        }
+    }
+
+    public static int getAantalPersonen() {
+        return aantalPersonen;
+    }
+
+    public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getMaandSalaris() {
+        return maandSalaris;
     }
 }
